24e35c3285716550fd64828824d5da511befa58b
Hennie <2749126@naver.com>
2021-06-21 02:22:57 +0900

    Controller
    update

--- src/main/java/com/example/student/StudentController.java
+++ src/main/java/com/example/student/StudentController.java
@@ -10,15 +10,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class StudentController {
-	@Inject
+	
+	@Inject // 의존 관계 주입
 	StudentDAO dao;
-	//   http://localhost/student/list
+	
+	// http://localhost/student/list
 	@RequestMapping("list.do")
 	public String list(Model model) {
-		//리스트 구하기
-		List<StudentDTO> list=dao.list();
+		// 리스트 구하기
+//		StudentDAO dao = new StudentDAO();
+		List<StudentDTO> list = dao.list();
 		
-		//자료 저장
+		// 자료 저장
 		model.addAttribute("list", list);
 		
 		return "list"; //   list.jsp로 출력
