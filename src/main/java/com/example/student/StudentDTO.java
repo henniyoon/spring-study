26e438047e7754cd507740c04750fc80d3fb6d9f
Hennie <2749126@naver.com>
2021-06-21 01:25:43 +0900

    Model - DTO
    - StudentDTO

---
+++ src/main/java/com/example/student/StudentDTO.java
@@ -0,0 +1,35 @@
+package com.example.student;
+
+public class StudentDTO {
+	private String student_no;
+	private String name;
+	private String major;
+	private int year;
+	
+	// Getter, Setter
+	public String getStudent_no() {
+		return student_no;
+	}
+	public void setStudent_no(String student_no) {
+		this.student_no = student_no;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getMajor() {
+		return major;
+	}
+	public void setMajor(String major) {
+		this.major = major;
+	}
+	public int getYear() {
+		return year;
+	}
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+}
