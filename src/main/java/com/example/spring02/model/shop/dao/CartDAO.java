54b98d4667688ff3aeeea2f92a3a1d6f78c6ca3b
Hennie <2749126@naver.com>
2021-07-06 22:48:14 +0900

    Shop : Model
    - CartDTO
    - CartDAO
    - CartDAOImpl

---
+++ src/main/java/com/example/spring02/model/shop/dao/CartDAO.java
@@ -0,0 +1,17 @@
+package com.example.spring02.model.shop.dao;
+
+import java.util.List;
+
+import com.example.spring02.model.shop.dto.CartDTO;
+
+public interface CartDAO {
+	public List<CartDTO> cartMoney();
+	public void insert(CartDTO dto);
+	public List<CartDTO> listCart(String userid);
+	public void delete(int cart_id);
+	public void deleteAll(String userid);
+	public int sumMoney(String userid);
+	// 장바구니 수량 변경
+	public void modifyCart(CartDTO dto);
+		
+}
